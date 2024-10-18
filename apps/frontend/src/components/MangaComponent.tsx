242283cbe7bb33d21ccbe61e12b706cac647ed91
Onesio Junior <onesiojr1@gmail.com>
2024-10-18 19:41:29 -0300

    feat: add update chapter to mangacomponent

--- apps/frontend/src/components/MangaComponent.tsx
+++ apps/frontend/src/components/MangaComponent.tsx
@@ -1,4 +1,6 @@
-'use client'
+"use client";
+import { useState } from "react";
+import { Backend_URL } from "../lib/Constants";
 import { Manga } from "../lib/types";
 import ClickButton from "./ClickButton";
 import Image from "next/image";
@@ -8,6 +10,21 @@ interface MangaComponentProps {
 }
 
 export default function MangaComponent(props: MangaComponentProps) {
+  const [chapter, setChapter] = useState(props.manga.chapter);
+  const addChapter = () => {
+    fetch(Backend_URL + `/manga/chapter/add/${props.manga.id}`, {
+      method: "PUT",
+    });
+    setChapter(chapter + 1);
+  };
+  
+  const substractChapter =  () => {
+    fetch(Backend_URL + `/manga/chapter/subtract/${props.manga.id}`, {
+      method: "PUT",
+    });
+    setChapter(chapter - 1);
+  };
+  
   return (
     <div className="flex flex-row justify-between items-center p-6 border border-slate-400 rounded-xl bg-gray-950">
       <div className="flex flex-col items-start sm:flex-row sm:items-center gap-6">
@@ -17,20 +34,18 @@ export default function MangaComponent(props: MangaComponentProps) {
           alt={props.manga.name}
           src={props.manga.image}
         />
-        <span className="text-4xl">
-          {props.manga.name}
-        </span>
+        <span className="text-4xl">{props.manga.name}</span>
       </div>
       <div className="flex gap-4 items-center font-3xl">
         <ClickButton
           variant={"tertiary"}
           size={"small"}
-          onClick={() => console.log("menos")}
+          onClick={substractChapter}
         >
           <span className="text-3xl">-</span>
         </ClickButton>
-        <span className="text-3xl">{props.manga.chapter}</span>
-        <ClickButton size={"small"} onClick={() => console.log("mais")}>
+        <span className="text-3xl">{chapter}</span>
+        <ClickButton size={"small"} onClick={addChapter}>
           <span className="text-3xl">+</span>
         </ClickButton>
       </div>
