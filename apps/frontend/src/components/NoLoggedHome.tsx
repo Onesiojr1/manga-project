48c31db108c4dcb43261a78b14546140a0006e75
Onesio Junior <onesiojr1@gmail.com>
2024-10-20 18:42:57 -0300

    fix image

--- apps/frontend/src/components/NoLoggedHome.tsx
+++ apps/frontend/src/components/NoLoggedHome.tsx
@@ -2,6 +2,7 @@ import LinkButton from "./LinkButton";
 import Container from "./Container";
 import Hero, { HeroSubtitle, HeroTitle } from "./Hero";
 import Image from "next/image";
+import heroImg from "../../public/img/hero.png"
 
 export default function NoLoggedHome() {
   return (
@@ -12,7 +13,7 @@ export default function NoLoggedHome() {
             <HeroTitle>A melhor forma de salvar<br /> o capitulo de seu manga favorito!</HeroTitle>
             <HeroSubtitle>Crie sua conta e comece agora!</HeroSubtitle>
             <LinkButton href="/signup" size={'large'}>Cadastrar</LinkButton>
-            <Image src="img/hero.png" alt="Hero image" className="mt-20"/>
+            <Image src={heroImg} alt="Hero image" className="mt-20" width={1080} height={1080}/>
           </Hero>
         </Container>
       </main>
